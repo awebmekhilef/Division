285bbd47a4e2657de19da626d1bbfd5667201530
Aweb Mekhilef <awebmekhilef@gmail.com>
2022-10-11 15:30:38 +1100

    Setup windowing and OpenGL context

--- Division/Source/Main.cpp
+++ Division/Source/Main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 720
 
 int main()
 {
-	std::cout << "Welcome to Division Engine!" << std::endl;
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Division", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
